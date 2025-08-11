0987bd744fd0441ea9aa4d02cc1c918074377e5b
Marco Andres Avendaño Suazo <mavendanosuazo@gmail.com>
2025-08-11 08:38:40 -0700

    feat(core/webapp): add script module

---
+++ src/core/webapp/script.js
@@ -0,0 +1,10 @@
+/**
+ * Returns the JavaScript code for the PT Worker web app.
+ * Note: This module currently contains a placeholder string.
+ * Copy the actual script from worker.js into this function.
+ */
+export function getWebAppScript() {
+  return '';
+}
+
+export default getWebAppScript;
